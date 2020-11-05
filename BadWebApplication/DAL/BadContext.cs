5c9c37be85bf04a84c03b541346debd05ee0e89b
bdijo <bdijo@D-IMEC-TRAINEE->
2020-11-05 16:30:47 +0200

    Configuring Data Access Layer

---
+++ BadWebApplication/DAL/BadContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BadWebApplication.DAL
+{
+    public class BadContext : DbContext
+    {
+        public BadContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
